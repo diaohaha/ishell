23f5aaae1e7f5cdbe2355d780c4b5552db260d9e
gaoda <gaoda@baidu.com>
2020-12-31 14:58:26 +0800

    add ffmopeg

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -7,3 +7,6 @@ ffmpeg -i output.mp4 -r 25 image-%05d.jpeg
 
 # 视频旋转90度
 ffmpeg -i input.mp4 -metadata:s:v rotate="90" -codec copy output.mp4
+
+#  将多个文件合并成一个文件
+ffmpeg -i "concat:1.ts|2.ts|3.ts" -acodec copy -vcodec copy final30.mp4
