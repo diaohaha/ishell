70355a0cd45d9739e5dedd4aa2f63b01b1da4e3d
gaoda <gaoda@baidu.com>
2021-02-19 15:20:01 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -8,3 +8,10 @@ mysql -h 127.0.0.1 -P 3306 -uroot -proot mydb -e "source tmp.sql;"
 
 # 查询第n高的工资
 select distinct(salary) from employee order by salary desc limit n-1,1;
+
+
+# 观察是否有大量线程处于不正常的状态或特征
+show processlist
+
+# 查看sql执行使用索引
+explain ****
