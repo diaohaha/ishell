4b7f3f8953a94a3804c14041fe69a0b7f4f0ea3a
gaoda <gaoda@baidu.com>
2021-02-19 15:14:43 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -6,3 +6,5 @@ mysqldump -h 127.0.0.1 -P 3306  -uroot -proot mydb t_my_table > tmp.sql;
 mysql -h 127.0.0.1 -P 3306 -uroot -proot mydb -e "source tmp.sql;"
 
 
+# 查询第n高的工资
+select distinct(salary) from employee order by salary desc limit n-1,1;
