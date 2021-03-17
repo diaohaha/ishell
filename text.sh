958cc62f5ade9e2ba37f651671e196d0254fa838
gaoda <gaoda@baidu.com>
2021-03-17 11:46:25 +0800

    Update text.sh

--- text.sh
+++ text.sh
@@ -10,3 +10,14 @@ find . -name "*.go" | xargs  sed -i "" 's/code/github/g'
 
 # 文件中多行合并成一行
 cat a.txt | tr '\n' ','
+
+
+# 求文件的交集 并集 差集
+
+# 交集
+sort a.txt b.txt | uniq -d
+# 并集
+sort a.txt b.txt | uniq 
+# 差集 a.txt-b.txt:
+sort a.txt b.txt b.txt | uniq -u
+
